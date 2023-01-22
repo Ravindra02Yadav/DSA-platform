c7825776870e4be9d43055ac3b6764fdf151632e
unknown <ravindra111223@gmail.com>
2023-01-22 13:00:06 +0530

    adding searchbar

--- frontend/src/components/Searchbar.jsx
+++ frontend/src/components/Searchbar.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 import countries from '../utils/country';
+import {useThrottle} from 'use-throttle'
 
 const Searchbar = () => {
+  
   const [inputText,setInputText] = useState("");
   const [suggestion,setSuggestion] = useState([]);
   const [active,setActive] = useState(0);
+  const scrollRef = useRef();
+
   const handleInput = (e) =>{
     setInputText(e.target.value)
   }
@@ -15,19 +19,37 @@ const Searchbar = () => {
     // console.log(e.keyCode)
     switch(e.keyCode){
       case 38:
+        if(active === 1){
+          scrollRef.current.scrollTop = suggestion.length * 42.677;
+          setActive(suggestion.length +1)
+        }
+        else if(active <= suggestion.length - 4){
+          scrollRef.current.scrollTop -= 42.677;
+        }
         setActive(prev =>prev -1 )
         break;
         case 40 :
-          setActive(prev =>prev +1 )
+          if(active === suggestion.length){
+            scrollRef.current.scrollTop = 0;
+            setActive(0)
+          }
+          else if(active >= 4){
+            scrollRef.current.scrollTop += 42.677;
+          }
+          
+            setActive(prev =>prev +1 )
+          
+          
           break;
           default :return;
     }
   }
+  const throttledText = useThrottle(inputText, 1500)
   useEffect(()=>{
-if(inputText == ""){
+if(throttledText == ""){
   setSuggestion([])
 }else{
-  let query = inputText.toLowerCase()
+  let query = throttledText.toLowerCase()
   // work for both capital and small letter 
   let newSuggestion = countries.filter(item=>{
     return item.country.toLowerCase().trim().indexOf(query) !== -1 ? true : false;
@@ -35,14 +57,14 @@ if(inputText == ""){
   // console.log(newSuggestion)
   setSuggestion(newSuggestion)
 }
-  },[inputText])
+  },[throttledText])
   // console.log(inputText)
   return (
     <Wrapper onKeyUp = {handleActiveSuggestion}>
 <SearchBarWrapper>
   <Input value = {inputText} onChange = {handleInput}/>
 </SearchBarWrapper>
-<SuggestionWrapper len = {5} active = {active}>
+<SuggestionWrapper len = {5} active = {active} ref = {scrollRef}>
   {suggestion.map((item,index)=>{
     return (
       <div key = {index} onMouseOver={()=>setActive(index + 1)}>
